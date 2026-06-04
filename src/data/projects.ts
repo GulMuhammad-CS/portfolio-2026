33d58156c5df92a997a252f2f83c81839140887a
Gul Muhammad <g.muhammad2912@gmail.com>
2026-06-04 23:04:12 +0100

    compartmentalizing code for projects

---
+++ src/data/projects.ts
@@ -0,0 +1,12 @@
+import { Project } from "@/types/project";
+
+export const projectsData: Project[] = [
+    {
+        id: "sexual-health-journal"
+        title: "NLP powered Sexual Health Journalling App"
+        description: "A journalling app that utilizes NLP models to identify key topics, layered with a sentiment analysis to provide necessary context, ultimately giving a layered feedback approach to the user. Also featuring various security features (encryption, on-device processing) accessibility features like high contrast colours for visually-impaired users, and a safety net to provide support for users going through hard times by identifying keywords in entries."
+        techStack: ["Java", "JavaFX", "Systems Architecture", "LangChain4J", "SQLite", "Jackson"]
+        githubUrl: 
+    }
+
+]
