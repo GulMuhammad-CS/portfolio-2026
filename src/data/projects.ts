6f5f252f1bc243823d0d37382d912f2b62967248
Gul Muhammad <g.muhammad2912@gmail.com>
2026-06-04 23:20:48 +0100

    cleaned projects array

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,11 +2,18 @@ import { Project } from "@/types/project";
 
 export const projectsData: Project[] = [
     {
-        id: "sexual-health-journal"
-        title: "NLP powered Sexual Health Journalling App"
-        description: "A journalling app that utilizes NLP models to identify key topics, layered with a sentiment analysis to provide necessary context, ultimately giving a layered feedback approach to the user. Also featuring various security features (encryption, on-device processing) accessibility features like high contrast colours for visually-impaired users, and a safety net to provide support for users going through hard times by identifying keywords in entries."
-        techStack: ["Java", "JavaFX", "Systems Architecture", "LangChain4J", "SQLite", "Jackson"]
-        githubUrl: 
+        id: "sexual-health-journal",
+        title: "NLP powered Sexual Health Journalling App",
+        description: "A journalling app that utilizes NLP models to identify key topics, layered with a sentiment analysis to provide necessary context, ultimately giving a layered feedback approach to the user. Also featuring various security features (encryption, on-device processing) accessibility features like high contrast colours for visually-impaired users, and a safety net to provide support for users going through hard times by identifying keywords in entries.",
+        techStack: ["Java", "JavaFX", "Systems Architecture", "LangChain4J", "SQLite", "Jackson", "css"],
+        githubUrl: "https://github.com/GulMuhammad-CS/Sexual-Health-Journal"
+    },
+    {
+        id: "ER/SPEAK-AAC",
+        title: "ER/SPEAK AAC - 2nd Place Winner",
+        description: "Designed for the MedTech Innovation programme, this prize-winnign alternative and augmentative communication (AAC) software is designed to provide an intuitive and non-triggering interface to allow non-verbal patients with ASD to communicate with healthcare providers.",
+        techStack: ["TypeScript", "css"],
+        githubUrl: "https://github.com/GulMuhammad-CS/ER-SPEAK"
     }
 
 ]
