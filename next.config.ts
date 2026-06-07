be9313ad6e8a323c975694481950b5b45bdc18c3
Gul Muhammad <g.muhammad2912@gmail.com>
2026-06-07 12:29:02 +0100

    implement section anchor and configuration routing redirect

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async redirects() {
+    return[
+      {
+        source: "/about",
+        destination: "/#about",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,6 +14,13 @@ export const projectsData: Project[] = [
         description: "Designed for the MedTech Innovation programme, this prize-winnign alternative and augmentative communication (AAC) software is designed to provide an intuitive and non-triggering interface to allow non-verbal patients with ASD to communicate with healthcare providers.",
         techStack: ["TypeScript", "css"],
         githubUrl: "https://github.com/GulMuhammad-CS/ER-SPEAK"
+    },
+    {
+        id: "Python Chessbot",
+        title: "Bitboard Chess Agent",
+        description: "Built using the chessmaker API in python, this chessbot translates the current board state into the bitboard format, giving it to the search engine which utilizes several AI search and optimization techniques (alpha-beta pruning, chacheiong, killer moves heuristics, etc). It finds the optimal move as deep as it can, translating it back and executing the move.",
+        techStack: ["Python"],
+        githubUrl: "https://github.com/GulMuhammad-CS/Chessbot_updated"
     }
 
 ]
